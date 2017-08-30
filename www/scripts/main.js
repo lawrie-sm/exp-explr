345ea33769c0cb45fcfa96d49b349cf9e2db1b2e
lawrie-sm <alertrelic@gmail.com>
2017-08-30 15:33:38 +0100

    Comments and ES6 syntax

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -6,28 +6,12 @@ const main = document.getElementsByTagName('main')[0];
 const constituencyURL = 'https://data.parliament.scot/api/MemberElectionConstituencyStatuses';
 const regionURL = 'https://data.parliament.scot/api/MemberElectionregionStatuses';
 const membersURL = 'https://data.parliament.scot/api/members';
-let mspList = [];
+
 let currentDate = new Date();
 
+/********************************************************************/
 
-/*********************************************/
-
-Promise.all([get(constituencyURL), get(regionURL), get(membersURL)])
-.then((dataArr) => {
-	
-	let electionResults = dataArr[0].concat(dataArr[1]);
-	mspList = getMSPDataFromElectionResults(electionResults);
-	
-	//console.log(mspList);
-	//console.log(dataArr[2]);
-	
-});
-
-
-
-/*********************************************/
-
-function get(url) {
+const get = (url) => {
 	return new Promise((resolve, reject) => {
 		fetch(url, {
 			method: 'get'
@@ -41,26 +25,59 @@ function get(url) {
 	});
 }
 
-function getMSPDataFromElectionResults(sourceArr)
-{
+//Returns array of current MSPs IDs based on SPs election results arrays 
+//and whether the election dates fall within current date
+const getMSPIDsFromElectionResults = (sourceArr) => {
 	let r = [];
 	sourceArr.forEach(function(elem) {
+
+		let startDate = null;
+		let endDate = null;
 		
+		startDate = getSPDateFromStr(elem.ValidFromDate);
 		
-		let dateStr = elem.ValidFromDate;
-		let T = dateStr.indexOf('T');
-		dateStr = dateStr.substring(0, T);
-		let dateArr = dateStr.split('-');
-		let year = dateArr[0], month = dateArr[1], day = dateArr[2];
+		if (elem.ValidUntilDate != null)
+		{
+			endDate = getSPDateFromStr(elem.ValidUntilDate);
+		}
 		
+		if ((currentDate >= startDate &&
+				 currentDate <= endDate) ||
+				(currentDate >= startDate &&
+				 endDate == null))
+		{
+			r.push(elem.PersonID);
+		}
 		
 	});
 	return r;
 }
 
+//Returns an array of full MSP objects, given an array of their IDs
+//and the full array of MSP objects
+const getMSPObjectsFromIDs = (mspIDList, mspObjects) => {
+	let r = [];
+	mspObjects.forEach(function(elem){
+		if (mspIDList.includes(elem.PersonID)) {
+			r.push(elem);
+		}
+	});
+	return r;
+}
 
-/*
-function addMemberEntry (member, i, arr) {
+//Processes an SP formatted date into a JS date object
+const getSPDateFromStr = (dateStr) => {
+		let T = dateStr.indexOf('T');
+		dateStr = dateStr.substring(0, T);
+		let dateArr = dateStr.split('-');
+		let year = dateArr[0], month = dateArr[1], day = dateArr[2];
+		return new Date(year, (month -1) , day);
+}
+
+//TODO:
+//https://stackoverflow.com/questions/40075726/whats-a-good-way-to-dynamically-render-dom-elements-with-plain-old-js
+
+const addMemberEntry = (member) => {
 	
 	var cols = document.createElement('div');
 	var cell = document.createElement('div');
@@ -81,4 +98,17 @@ function addMemberEntry (member, i, arr) {
 	main.appendChild(cell);
 	
 }
-*/
+
+/********************************************************************/
+
+Promise.all([get(constituencyURL), get(regionURL), get(membersURL)])
+.then((dataArr) => {
+	
+	let electionResults = dataArr[0].concat(dataArr[1]);
+	let mspIDList = getMSPIDsFromElectionResults(electionResults);
+	let mspList = getMSPObjectsFromIDs(mspIDList, dataArr[2]);
+	
+	mspList.forEach(function(elem) {
+		addMemberEntry(elem);
+	});
+});
