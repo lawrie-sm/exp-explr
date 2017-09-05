e4ea4a77ebff84b4655d80f1ea6ffd647699521d
lawrie-sm <alertrelic@gmail.com>
2017-09-05 14:02:28 +0100

    More progress with webpack, polyfills, tree shaking and minifier

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: ['promise-polyfill', 'whatwg-fetch', './src/scripts/app.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+	module: {
+		loaders: [
+			{
+				test:/\.js$/,
+				loader: 'babel-loader',
+				query: { presets: ['env'] }
+			}
+		]
+	},
+	plugins: [ new UglifyJSPlugin()]
+};
