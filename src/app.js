222e71f6386c1c70c7257cc97387b5f24e97d2ea
lawrie-sm <alertrelic@gmail.com>
2017-09-06 10:59:39 +0100

    Got webpack-dev-server running

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ model.getDataFromAPIs.then((d) => {
 	
 	let mspMap = model.getMSPMap(
 		d.constitResults, d.regResults, d.constits, d.regions);
-		
+
 	model.addMSPData(mspMap, d.basicMSPData);
 	
 	view.setView(mspMap);
--- webpack.dev-config.js
+++ webpack.dev-config.js
@@ -3,6 +3,10 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: ['./src/app.js'],
+	devtool: 'inline-source-map',
+	 devServer: {
+     contentBase: './dist'
+   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
