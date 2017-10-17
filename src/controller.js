d5828259c52f27a1c67d95b9a1b5651bb9eb8838
lawrie-sm <alertrelic@gmail.com>
2017-10-17 11:13:38 +0100

    Govt bug fix for demo

--- src/controller.js
+++ src/controller.js
@@ -1,23 +1,21 @@
-'use strict';
 import * as model from './model';
 import * as view from './view';
 
 const CURRENT_DATE = new Date();
 
-export const getExpandedCellData = (date) => {
-	return new Promise((resolve, reject) => {
-		let mspMap = model.getExpandedMSPMap(date).then((mspMap) => {
-			resolve(mspMap);
-		});
-	});
-}
+export const getExpandedCellData = date => new Promise((resolve, reject) => {
+  const mspMap = model
+    .getExpandedMSPMap(date).then((mspMap) => {
+      resolve(mspMap);
+    });
+});
 
 export const refreshView = (date, groupBy) => {
-		model.getMSPMap(date).then((mspMap) => {
-			view.refreshCells(mspMap, date, groupBy);
-		});
-}
+  model.getMSPMap(date).then((mspMap) => {
+    view.refreshCells(mspMap, date, groupBy);
+  });
+};
 
-/*** Main ***/
+//  Main
 view.init(CURRENT_DATE);
 refreshView(CURRENT_DATE, 'party');
--- src/objs.js
+++ src/objs.js
@@ -1,46 +1,46 @@
 export function Area(_name, _code) {
-	this.name = _name;
-	this.code = _code;
+  this.name = _name;
+  this.code = _code;
 }
 
 export function Party(_name, _abbreviation) {
-	this.name = _name;
-	this.abbreviation = _abbreviation;
+  this.name = _name;
+  this.abbreviation = _abbreviation;
 }
 
-export function Role(_name, _rank, _altText) {	
-	this.name = _name;
-	this.rank = _rank;
-	this.altText = _altText;
+export function Role(_name, _rank, _altText) {
+  this.name = _name;
+  this.rank = _rank;
+  this.altText = _altText;
 }
 
 export function Address(_type, _street, _postCode, _region, _town) {
-	this.type = _type;
-	this.street = _street;
-	this.postCode = _postCode;
-	this.region = _region;
-	this.town = _town;
+  this.type = _type;
+  this.street = _street;
+  this.postCode = _postCode;
+  this.region = _region;
+  this.town = _town;
 }
 
-export function Contact(_type, _value) {	
-	this.type = _type;
-	this.value = _value;
+export function Contact(_type, _value) {
+  this.type = _type;
+  this.value = _value;
 }
 
 export function MSP(_ID, _constit, _region, _firstName, _lastName, _DOB, _photoURL, _party) {
-	this.constit = _constit;
-	this.region = _region;
-	this.firstName = _firstName;
-	this.lastName = _lastName;
-	this.DOB = _DOB;
-	this.photoURL = _photoURL;
-	this.party = _party;
-	this.partyRoles = [];
-	this.govtRoles = [];
-	this.committeeRoles = [];
-	this.cpgRoles = [];
-	this.addresses = [];
-	this.emails = [];
-	this.telephones = [];
-	this.websites = [];
+  this.constit = _constit;
+  this.region = _region;
+  this.firstName = _firstName;
+  this.lastName = _lastName;
+  this.DOB = _DOB;
+  this.photoURL = _photoURL;
+  this.party = _party;
+  this.partyRoles = [];
+  this.govtRoles = [];
+  this.committeeRoles = [];
+  this.cpgRoles = [];
+  this.addresses = [];
+  this.emails = [];
+  this.telephones = [];
+  this.websites = [];
 }
